46cc3ea1c07daac8d463b1499ae6366a7eff9855
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-04-07 23:51:06 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+﻿function drawTree(n) {
+    for (var i = 1; i < n; i++) {
+        
+        var space = ' ';
+        var star = "*";
+
+        for (var j = 1; j < n-i; j++) {
+            space += ' ';
+        }
+
+        for (var j = 1; j < i*2-1; j++) {
+            star += '*';
+        }
+
+    console.log(space, star);
+    }
+}
+
+drawTree(10);
